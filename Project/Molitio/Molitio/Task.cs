a3b6205792d3cdcb26c0bf73f5c31dcdcf086576
hieraar <hiera.ahm2002@mail.ugm.ac.id>
2023-09-21 10:46:51 +0700

    Adding class PomodoroType, Task, ToDoList, Notes

---
+++ Project/Molitio/Molitio/Task.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Molitio
+{
+    class Task
+    {
+        public int idTask
+        {
+            get { return idTask; }
+        }
+
+        public string taskName
+        {
+            get { return taskName; }
+        }
+
+        public DateTime taskDeadlineDate
+        {
+            get { return taskDeadlineDate; }
+        }
+
+        public int taskDeadlineTime
+        {
+            get { return taskDeadlineTime; }
+        }
+
+        public string taskDescription
+        {
+            get { return taskDescription; }
+        }
+    }
+}
--- Project/Molitio/Molitio/obj/Debug/net6.0-windows/App.g.i.cs
+++ Project/Molitio/Molitio/obj/Debug/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace Molitio {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.8.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Molitio {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.8.0")]
         public static void Main() {
             Molitio.App app = new Molitio.App();
             app.InitializeComponent();
