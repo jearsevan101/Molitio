d6c166abb9fb4e8d9976ea2afdcf8e0a2ad3b1a8
jearsevan101 <juliusevanars@gmail.com>
2023-10-29 09:44:40 +0700

    fix error2

--- Project/Molitio/Molitio/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ Project/Molitio/Molitio/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -78,7 +78,7 @@ namespace Molitio {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Molitio;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Molitio;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
