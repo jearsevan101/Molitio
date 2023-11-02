8958cb7c8c21e778cdbf0506a80b0f270793209c
leleonnn <leonardmars63@gmail.com>
2023-11-02 15:19:33 +0700

    readd pop up pages

---
+++ Project/Molitio/Molitio/ProjectName.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Molitio
+{
+    /// <summary>
+    /// Interaction logic for ProjectName.xaml
+    /// </summary>
+    public partial class ProjectName : Window
+    {
+        public ProjectName()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
