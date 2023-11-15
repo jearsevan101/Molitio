bdd8d39e0d04ff2dbdc41db9e49065ae0f96251b
hieraar <hiera.ahm2002@mail.ugm.ac.id>
2023-11-16 01:40:13 +0700

    Menambahkan akses memutar lagu dengan penyimpanan cloud blob storage

--- Project/Molitio/Molitio/MVVM/View/ProductivityView.xaml.cs
+++ Project/Molitio/Molitio/MVVM/View/ProductivityView.xaml.cs
@@ -20,9 +20,36 @@ namespace Molitio.MVVM.View
     /// </summary>
     public partial class ProductivityView : UserControl
     {
+        private SoundController soundController;
         public ProductivityView()
         {
             InitializeComponent();
+            soundController = new SoundController(mediaElement);
+        }
+
+        private void btStop_Click(object sender, RoutedEventArgs e)
+        {
+            soundController.Stop();
+        }
+
+        private void btFirePlace_Click(object sender, RoutedEventArgs e)
+        {
+            soundController.Play("https://cs1100320016d775b19.blob.core.windows.net/junpro/Fireplace.wav?sv=2023-01-03&st=2023-11-15T18%3A19%3A46Z&se=2024-11-16T18%3A19%3A00Z&sr=b&sp=r&sig=vemBgEjssTKhUcYjiwP6ut9hTpFkEMXa5euC1PTu5u0%3D");
+        }
+
+        private void btForrest_Click(object sender, RoutedEventArgs e)
+        {
+            soundController.Play("https://cs1100320016d775b19.blob.core.windows.net/junpro/Forrest.wav?sv=2023-01-03&st=2023-11-15T18%3A20%3A20Z&se=2024-11-16T18%3A20%3A00Z&sr=b&sp=r&sig=1JDdSuLPNn9vRGv6hFMQR%2F7k2ujiIT1klUVVfO4oVRQ%3D");
+        }
+
+        private void btOcean_Click(object sender, RoutedEventArgs e)
+        {
+            soundController.Play("https://cs1100320016d775b19.blob.core.windows.net/junpro/Ocean.wav?sv=2023-01-03&st=2023-11-15T17%3A49%3A11Z&se=2024-11-16T17%3A49%3A00Z&sr=b&sp=r&sig=ROeSgiH%2B%2BQ3N32bWttGAweTCFNY7xEb0XuuYq4i2yos%3D");
+        }
+
+        private void btLofi_Click(object sender, RoutedEventArgs e)
+        {
+            soundController.Play("https://cs1100320016d775b19.blob.core.windows.net/junpro/Lofi.wav?sv=2023-01-03&st=2023-11-15T18%3A20%3A44Z&se=2024-11-16T18%3A20%3A00Z&sr=b&sp=r&sig=%2FfEKmydPYrexASuKEj152JHYHowzstvTe10TDY3ZpVc%3D");
         }
     }
 }
---
+++ Project/Molitio/Molitio/SoundController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Molitio
+{
+    public class SoundController
+    {
+        private MediaElement mediaElement;
+
+        public SoundController(MediaElement mediaElement)
+        {
+            this.mediaElement = mediaElement;
+        }
+
+        public void Play(string audioUrl)
+        {
+            mediaElement.Source = new Uri(audioUrl, UriKind.Absolute);
+            mediaElement.Play();
+        }
+
+        public void Stop()
+        {
+            mediaElement.Stop();
+        }
+    }
+}
